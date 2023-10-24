9eddd782d04fb09186e6130d9d5a61b3cb9e4702
skayloo <skayloo@yandex.ru>
2023-10-24 22:43:34 +0300

    chto-to na neponyatnom 3

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -3,7 +3,7 @@ set -xe
 sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
 sudo rm -r /opt/sausage-store/static/sausage-store/frontend || true
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz ${NEXUS_REPO_URL_FRONTEND}std-022-039-frontend/${VERSION}/sausage-store-${VERSION}.tar.gz
-tar -zxf sausage-store.tar.gz --directory /opt/sausage-store/static/sausage-store/||true
+sudo tar -zxf sausage-store.tar.gz --directory /opt/sausage-store/static/sausage-store/||true
 sudo chmod 755 /opt/sausage-store/static/sausage-store/frontend
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
