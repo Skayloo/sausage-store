dcf4e3f508dcc914f5fde0d488f4f39eb177bc56
skayloo <skayloo@yandex.ru>
2023-10-24 22:20:53 +0300

    add front deploy

---
+++ frontend/deploy.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+set -xe
+sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
+sudo rm -r /opt/sausage-store/static/sausage-store/frontend
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz ${NEXUS_REPO_URL_FRONTEND}std-022-039-frontend/std-022-039-frontend/std-022-039-frontend/${VERSION}/sausage-store-${VERSION}.tar.gz
+tar -zxf sausage-store.tar.gz --directory /opt/sausage-store/static/sausage-store/||true
+sudo chmod 755 /opt/sausage-store/static/sausage-store/frontend
+sudo systemctl daemon-reload
+#Перезапускаем сервис сосисочной
+sudo systemctl restart sausage-store-frontend --now
