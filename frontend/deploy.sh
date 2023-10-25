7049895d433dcdb27b336a2d04a2ef6f208312a6
skayloo <skayloo@yandex.ru>
2023-10-25 09:09:27 +0300

    update front perositories

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -4,7 +4,8 @@ sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-fro
 sudo rm -r /opt/sausage-store/static/sausage-store/frontend || true
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz ${NEXUS_REPO_URL_FRONTEND}std-022-039-frontend/${VERSION}/sausage-store-${VERSION}.tar.gz
 sudo tar -zxf sausage-store.tar.gz --directory /opt/sausage-store/static/sausage-store/ || true
+#Это я на всякий случай (понятия не имею какие права выдает разархивирование)
 sudo chmod 755 /opt/sausage-store/static/sausage-store/frontend || true
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
-sudo systemctl restart sausage-store-frontend --now
+sudo systemctl restart sausage-store-frontend --now 
