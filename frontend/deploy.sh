3d7258c211bcf66bbba2d3c3cf91d873f5324135
skayloo <skayloo@yandex.ru>
2024-01-10 20:52:47 +0300

    fixing backend 6

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -3,4 +3,4 @@ set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-frontend || true
-sudo docker run --restart=on-failure:10 -p 8080:80 -d --name sausage-frontend --network=sausage_network "${CI_REGISTRY_IMAGE}"/sausage-frontend:"${VERSION}"
+sudo docker run --restart=on-failure:10 -p 8080:80 -v /tmp/${CI_PROJECT_DIR}/frontend/default.conf:/etc/nginx/conf.d/default.conf -d --name sausage-frontend --network=sausage_network "${CI_REGISTRY_IMAGE}"/sausage-frontend:"${VERSION}"
