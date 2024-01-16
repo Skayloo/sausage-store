db9a691fd981ce1c754306f20de60de2017391fa
skayloo <skayloo@yandex.ru>
2024-01-16 16:44:18 +0300

    Minio s3

--- backend/src/main/java/com/yandex/practicum/devops/SausageApplication.java
+++ backend/src/main/java/com/yandex/practicum/devops/SausageApplication.java
@@ -19,13 +19,13 @@ public class SausageApplication {
     @Bean
     CommandLineRunner runner(ProductService productService) {
         return args -> {
-            productService.save(new Product(1L, "Сливочная", 320.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg"));
-            productService.save(new Product(2L, "Особая", 179.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg"));
-            productService.save(new Product(3L, "Молочная", 225.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg"));
-            productService.save(new Product(4L, "Нюренбергская", 315.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg"));
-            productService.save(new Product(5L, "Мюнхенская", 330.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg"));
-            productService.save(new Product(6L, "Еврейская", 189.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg"));
-            productService.save(new Product(6L, "Американская", 189.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg"));
+            productService.save(new Product(1L, "Сливочная", 320.00, "https://storage.yandexcloud.net/minio-std-022-039/6.jpg"));
+            productService.save(new Product(2L, "Особая", 179.00, "https://storage.yandexcloud.net/minio-std-022-039/5.jpg"));
+            productService.save(new Product(3L, "Молочная", 225.00, "https://storage.yandexcloud.net/minio-std-022-039/4.jpg"));
+            productService.save(new Product(4L, "Нюренбергская", 315.00, "https://storage.yandexcloud.net/minio-std-022-039/3.jpg"));
+            productService.save(new Product(5L, "Мюнхенская", 330.00, "https://storage.yandexcloud.net/minio-std-022-039/2.jpg"));
+            productService.save(new Product(6L, "Еврейская", 189.00, "https://storage.yandexcloud.net/minio-std-022-039/1.jpg"));
+            productService.save(new Product(7L, "Американская", 189.00, "https://storage.yandexcloud.net/minio-std-022-039/1.jpg"));
         };
     }
 }
